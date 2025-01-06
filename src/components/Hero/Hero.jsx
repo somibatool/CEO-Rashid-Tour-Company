0236582d1332fed4871ab22355d5909e27e571df
somi batool <somibatool44@gmail.com>
2025-01-06 14:52:10 +0500

    Travel website Pakistani Mountain Tour commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+      {/* <div className='mb-5'>
+        <h1 className='text-white text-center'>Embrace the North: Mountain Peaks & Autumn Dreams</h1>
+        <p className='text-white text-center'>Discover the magic of the north with our expertly crafted tours. From snow-covered trails to the vibrant hues of autumn, we curate unforgettable experiences that connect you with the raw beauty of the region. Loremplaceat dolorem. Inventore obcaecati nulla dicta, fugit error, eligendieum consequatur, pariatur expedita quam sapiente magni magnam?
+         
+      </p>
+      </div> */}
+
+    </>
+  )
+}
+
+export default Hero

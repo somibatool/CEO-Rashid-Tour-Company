acd3a98a423d07b7829dcbbbe9fd71c34a557808
somi batool <somibatool44@gmail.com>
2025-01-25 21:28:02 +0500

    last commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,9 +5,10 @@ const Hero = () => {
     <>
      <div className="flex items-center justify-center h-screen bg-cover bg-center"> 
             <div className="text-center transform -translate-y-10"> 
-              <h1 className="text-white text-5xl leading-tight mb-6"> 
-              Explore the World, One Adventure at a Time, Discover Hidden Gems, Create Unforgettable Memories, and Embrace the Journey
-              </h1>
+            <h1 className="text-white text-3xl lg:text-5xl leading-tight mb-6">
+                Explore the World, One Adventure at a Time, Discover Hidden Gems, Create Unforgettable Memories, and Embrace the Journey.
+            </h1>
+
              
             </div>
       </div> 
